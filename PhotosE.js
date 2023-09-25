d53d819c179eb84934e75d0b40dcb235a8a97ba6
omarbarbeir <138704845+omarbarbeir@users.noreply.github.com>
2023-09-26 00:38:24 +0300

    Add files via upload

---
+++ PhotosE.js
@@ -0,0 +1,454 @@
+let players = [
+    {
+        img:"./img e/asenso.jpeg",
+        name:"ماركو اسينسيو"
+    },
+    {
+        img:"./img e/bale.jpeg",
+        name:"جاريث بيل"
+    },
+    {
+        img:"./img e/beckham.jpeg",
+        name:"دافيد بيكهام"
+    },
+    {
+        img:"./img e/bellengham.jpeg",
+        name:"جود بيلنجهام"
+    },
+    {
+        img:"./img e/benzema.jpeg",
+        name:"كريم بنزيما"
+    },
+    {
+        img:"./img e/berba.jpeg",
+        name:"بيرباتوف"
+    },
+    {
+        img:"./img e/bernardo.jpeg",
+        name:"برناردو سيلفا"
+    },
+    {
+        img:"./img e/besaka.jpeg",
+        name:"وان بيساكا"
+    },
+    {
+        img:"./img e/bruno.jpeg",
+        name:"برونو فرنانديز"
+    },
+    {
+        img:"./img e/buffon.jpeg",
+        name:"بوفون"
+    },
+    {
+        img:"./img e/cama.jpeg",
+        name:"كامافينجا"
+    },
+    {
+        img:"./img e/cancelo.jpeg",
+        name:"جواو كانسيلو"
+    },
+    {
+        img:"./img e/chel.jpeg",
+        name:"كيلليني"
+    },
+    {
+        img:"./img e/costa.jpeg",
+        name:"دييجو كوستا"
+    },
+    {
+        img:"./img e/cot.jpeg",
+        name:"كوتينيو"
+    },
+    {
+        img:"./img e/davids.jpeg",
+        name:"ايدجر دافيدز"
+    },
+    {
+        img:"./img e/davis.jpeg",
+        name:"الفونسو ديفيز"
+    },
+    {
+        img:"./img e/dona.jpeg",
+        name:"دوناروما"
+    },
+    {
+        img:"./img e/dsilva.jpeg",
+        name:"دافيد سيلفا"
+    },
+    {
+        img:"./img e/emillano.jpeg",
+        name:"ايميليانو مارتينيز"
+    },
+    {
+        img:"./img e/enzo.jpeg",
+        name:"إنزو فرنانديز"
+    },
+    {
+        img:"./img e/eriksen.jpeg",
+        name:"إيركسن"
+    },
+    {
+        img:"./img e/fati.jpeg",
+        name:"انسو فاتي"
+    },
+    {
+        img:"./img e/figo.jpeg",
+        name:"لويس فيجو"
+    },
+    {
+        img:"./img e/firmino.jpeg",
+        name:"فيرمينيو"
+    },
+    {
+        img:"./img e/foden.jpeg",
+        name:"فودين"
+    },
+    {
+        img:"./img e/ford.jpeg",
+        name:"بيكفورد"
+    },
+    {
+        img:"./img e/forlan.jpeg",
+        name:"دييجو فورلان"
+    },
+    {
+        img:"./img e/gerard.jpeg",
+        name:"ستيفين جيرارد"
+    },
+    {
+        img:"./img e/gigs.jpeg",
+        name:"جيجز"
+    },
+    {
+        img:"./img e/gonzalo.jpeg",
+        name:"جونزالو هيجوايين"
+    },
+    {
+        img:"./img e/grelesh.jpeg",
+        name:"جاك جريليش"
+    },
+    {
+        img:"./img e/haaland.jpeg",
+        name:"هالاند"
+    },
+    {
+        img:"./img e/harry.jpeg",
+        name:"هاري ماجواير"
+    },
+    {
+        img:"./img e/hart.jpeg",
+        name:"جو هارت"
+    },
+    {
+        img:"./img e/havertz.jpeg",
+        name:"كاي هافيرتز"
+    },
+    {
+        img:"./img e/hazard.jpeg",
+        name:"ايدين هازارد"
+    },
+    {
+        img:"./img e/hendo.jpeg",
+        name:"جوردان هندرسون"
+    },
+    {
+        img:"./img e/henry.jpeg",
+        name:"تيري هينري"
+    },
+    {
+        img:"./img e/hernandez.jpeg",
+        name:"خافيير هيرنانديز"
+    },
+    {
+        img:"./img e/hulk.jpeg",
+        name:"هالك"
+    },
+    {
+        img:"./img e/ibra.jpeg",
+        name:"ابراهيموفيتش"
+    },
+    {
+        img:"./img e/inesta.jpeg",
+        name:"انييستا"
+    },
+    {
+        img:"./img e/jamal.jpeg",
+        name:"جمال موسيالا"
+    },
+    {
+        img:"./img e/joao.jpeg",
+        name:"جواو فيليكس"
+    },
+    {
+        img:"./img e/kaka.jpeg",
+        name:"ريكاردو كاكا"
+    },
+    {
+        img:"./img e/kane.jpeg",
+        name:"هاري كين"
+    },
+    {
+        img:"./img e/kante.jpeg",
+        name:"نجولو كانتي"
+    },
+    {
+        img:"./img e/kavara.jpeg",
+        name:"كفاراتسخيليا"
+    },
+    {
+        img:"./img e/kdb.jpeg",
+        name:"كيفين دي بروين"
+    },
+    {
+        img:"./img e/konde.jpeg",
+        name:"كوندي"
+    },
+    {
+        img:"./img e/lampard.jpeg",
+        name:"فرانك لامبارد"
+    },
+    {
+        img:"./img e/leao.jpeg",
+        name:"رفائيل لياو"
+    },
+    {
+        img:"./img e/lewa.jpeg",
+        name:"ليفاندوفسكي"
+    },
+    {
+        img:"./img e/mane.jpeg",
+        name:"ساديو ماني"
+    },
+    {
+        img:"./img e/maradona.jpeg",
+        name:"مارادونا"
+    },
+    {
+        img:"./img e/matip.jpeg",
+        name:"ماتيب"
+    },
+    {
+        img:"./img e/mbappe.jpeg",
+        name:"مبابي"
+    },
+    {
+        img:"./img e/messi.jpeg",
+        name:"ميسي"
+    },
+    {
+        img:"./img e/milner.jpeg",
+        name:"ميلنر"
+    },
+    {
+        img:"./img e/morata.jpeg",
+        name:"موراتا"
+    },
+    {
+        img:"./img e/mount.jpeg",
+        name:"ماونت"
+    },
+    {
+        img:"./img e/muller.jpeg",
+        name:"توماس مولر"
+    },
+    {
+        img:"./img e/neves.jpeg",
+        name:"روبن نيفيز"
+    },
+    {
+        img:"./img e/neymar.jpeg",
+        name:"نيمار"
+    },
+    {
+        img:"./img e/nicolo.jpeg",
+        name:"باريلا"
+    },
+    {
+        img:"./img e/nuer.jpeg",
+        name:"نوير"
+    },
+    {
+        img:"./img e/odegard.jpeg",
+        name:"اوديجارد"
+    },
+    {
+        img:"./img e/ole.jpeg",
+        name:"سولشاير"
+    },
+    {
+        img:"./img e/ousman.jpeg",
+        name:"عثمان ديمبيلي"
+    },
+    {
+        img:"./img e/oxlade.jpeg",
+        name:"شامبرلين"
+    },
+    {
+        img:"./img e/ozil.jpeg",
+        name:"مسعود اوزيل"
+    },
+    {
+        img:"./img e/pele.jpeg",
+        name:"بيليه"
+    },
+    {
+        img:"./img e/perlo.jpeg",
+        name:"بيرلو"
+    },
+    {
+        img:"./img e/pogba.jpeg",
+        name:"بوجبا"
+    },
+    {
+        img:"./img e/r9.jpeg",
+        name:"رونالدو"
+    },
+    {
+        img:"./img e/racee.jpeg",
+        name:"ريس جيمس"
+    },
+    {
+        img:"./img e/ramos.jpeg",
+        name:"راموس"
+    },
+    {
+        img:"./img e/ramsdal.jpeg",
+        name:"رامسديل"
+    },
+    {
+        img:"./img e/raphiena.jpeg",
+        name:"رافينيا"
+    },
+    {
+        img:"./img e/rashford.jpeg",
+        name:"راشفورد"
+    },
+    {
+        img:"./img e/kun.webp",
+        name:"اجويرو"
+    },
+    {
+        img:"./img e/reo.jpeg",
+        name:"ريو فرديناند"
+    },
+    {
+        img:"./img e/reus.jpeg",
+        name:"رويس"
+    },
+    {
+        img:"./img e/rice.jpeg",
+        name:"ديكلان رايس"
+    },
+    {
+        img:"./img e/robben.jpeg",
+        name:"روبن"
+    },
+    {
+        img:"./img e/robertson.jpeg",
+        name:"روبرتسون"
+    },
+    {
+        img:"./img e/rodryego.jpeg",
+        name:"رودريجو"
+    },
+    {
+        img:"./img e/ronaldo.jpeg",
+        name:"كرستيانو"
+    },
+    {
+        img:"./img e/roone.jpeg",
+        name:"روني"
+    },
+    {
+        img:"./img e/saka.jpeg",
+        name:"ساكا"
+    },
+    {
+        img:"./img e/salah.jpeg",
+        name:"محمد صلاح"
+    },
+    {
+        img:"./img e/sancho.jpeg",
+        name:"سانشو"
+    },
+    {
+        img:"./img e/savic.jpeg",
+        name:"سافيتش"
+    },
+    {
+        img:"./img e/scholes.jpeg",
+        name:"بول سكولز"
+    },
+    {
+        img:"./img e/sheva.jpeg",
+        name:"شيفشينكو"
+    },
+    {
+        img:"./img e/silva.jpeg",
+        name:"تياجو سيلفا"
+    },
+    {
+        img:"./img e/son.jpeg",
+        name:"سون"
+    },
+    {
+        img:"./img e/suarez.jpeg",
+        name:"لويس سواريز"
+    },
+    {
+        img:"./img e/ter.jpeg",
+        name:"تيرشتيجن"
+    },
+    {
+        img:"./img e/terry.jpeg",
+        name:"جون تيري"
+    },
+    {
+        img:"./img e/teves.jpeg",
+        name:"تيفيز"
+    },
+    {
+        img:"./img e/thiago.jpeg",
+        name:"تياجو الكنتارا"
+    },
+    {
+        img:"./img e/trent.jpeg",
+        name:"ارنولد"
+    },
+    {
+        img:"./img e/van dijk.jpeg",
+        name:"فان دايك"
+    },
+    {
+        img:"./img e/van.jpeg",
+        name:"فان بيرسي"
+    },
+    {
+        img:"./img e/varan.jpeg",
+        name:"فاران"
+    },
+    {
+        img:"./img e/vini.jpeg",
+        name:"فينسيوس"
+    },
+    {
+        img:"./img e/vlaho.jpeg",
+        name:"فلاهوفيتش"
+    },
+    {
+        img:"./img e/walcot.jpeg",
+        name:"ثيو والكوت"
+    },
+    {
+        img:"./img e/xavi.jpeg",
+        name:"تشافي"
+    },
+    {
+        img:"./img e/zidan.jpeg",
+        name:"زيدان"
+    },
+    {
+        img:"./img e/zin.jpeg",
+        name:"زنشينكو"
+    },
+]
